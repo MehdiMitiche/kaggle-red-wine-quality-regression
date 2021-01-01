367b3ec05016f59152b7d202b4a94043416cd28a
Mehdi Mitiche <gm_mitiche@esi.dz>
2021-01-01 20:55:51 +0100

    Create README.m

---
+++ README.m
@@ -0,0 +1,3 @@
+Nueral network made with PyTorch to predict the red wine quality, the dataset used can be find here : https://www.kaggle.com/uciml/red-wine-quality-cortez-et-al-2009
+
+ps: wine is not good for your health! this problem was treated just for learning purposes !
